865ee9e21dc74770cfcefb2b9c287ffca48d0c14
Taguchi-ivi <k.taguchi@ivisystem.com>
2022-11-30 15:30:31 +0900

    画面表示はOK DBを作成できない

--- backend/docker/web/start-server.sh
+++ backend/docker/web/start-server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-echo "run start-server.sh" &
-rails server
+# echo "run start-server.sh" &
+# rails server
 
 # TCP通信の必要がなくなったため、rails serverコマンドからポートとバインドの設定を削除
-# rails server -p 3000 -b 0.0.0.0
+echo "run start-server.sh" &
+rails server -p 3000 -b 0.0.0.0
