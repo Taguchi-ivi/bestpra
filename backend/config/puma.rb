6bf24a0177732b695d91659860f268dd7d3012cf
Taguchi-ivi <k.taguchi@ivisystem.com>
2022-12-12 11:29:31 +0900

    nginx OK

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -15,7 +15,8 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# switch
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,3 +42,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+root = File.expand_path("../..", __FILE__)
+bind "unix://#{root}/tmp/sockets/puma.sock"
