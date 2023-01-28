60b08466ffe2752d4cb77f1b646c565f6cdac30a
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-01-28 12:49:48 +0900

    firebase.js fix

--- frontend/plugins/firebase.js
+++ frontend/plugins/firebase.js
@@ -8,23 +8,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.FIREBASE_PROJECTID,
-//     storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-//     messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-//     appId: process.env.FIREBASE_APPID,
-//     measurementId: process.env.FIREBASE_MEASUREMENTID
-// };
 const firebaseConfig = {
-    apiKey: "AIzaSyAY_3Si7KS5mPYz7a1pilG6vbFqLuNVz0o",
-    authDomain: "bestpra-auth.firebaseapp.com",
-    projectId: "bestpra-auth",
-    storageBucket: "bestpra-auth.appspot.com",
-    messagingSenderId: "1036427310673",
-    appId: "1:1036427310673:web:9731c3873564a966fc6a70",
-    measurementId: "G-DJD4Y5RVW6"
+    apiKey: process.env.ApiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
