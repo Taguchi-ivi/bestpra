aafff9b4d031a8dd4e9e73de88e3993c057504fe
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-02-03 16:55:27 +0900

    session ng & before pwa add

--- frontend/helpers/index.js
+++ frontend/helpers/index.js
@@ -1,9 +1,15 @@
-import jwtDecode from 'jwt-decode'
+// import { auth } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+// import admin from "firebase-admin";
+// import jwtDecode from 'jwt-decode'
 // import cookieparser from 'cookie-parser'
 // const cookieParser = require('cookie-parser')
 const cookieParser = require('cookieparser')
 
 export function getUserFromCookie(req) {
+
+
     // console.log('level1---------')
     if (process.server && process.static) return
     // console.log('level2---------')
@@ -16,19 +22,37 @@ export function getUserFromCookie(req) {
         // console.log('level5---------')
         // console.log(parsed)
         const accessTokenCookie = parsed.access_token
-        // console.log('level6---------')
-        console.log(accessTokenCookie)
+        console.log('level6---------')
+        // console.log(accessTokenCookie)
         if (!accessTokenCookie) return
 
         // console.log('level7---------')
         // console.log(accessTokenCookie)
-        const decodeToken = jwtDecode(accessTokenCookie)
+        // const decodeToken = jwtDecode(accessTokenCookie)
+        // auth()
+        getAuth().verifyIdToken(accessTokenCookie)
+            .then((decodedToken) => {
+                // const uid = decodedToken.uid;
+                console.log('ここにdecode出力')
+                console.log(decodedToken)
+
+                // console.log('level8---------')
+                if(!decodedToken) return
+
+                // console.log('level9---------')
+                // console.log(decodeToken)
+                return decodedToken
+            })
+            .catch((error) => {
+                // Handle error
+                console.log(error)
+            });
         // console.log('level8---------')
-        if(!decodeToken) return
+        // if(!decodeToken) return
 
         // console.log('level9---------')
         // console.log(decodeToken)
-        return decodeToken
+        // return decodeToken
     }
 }
 
