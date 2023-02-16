846853270e6fb54c8767dbbe1782b11d74e4eba3
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-02-16 16:22:23 +0900

    add auth token controller test

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -33,6 +33,9 @@ module App
     # $LOAD_PATHにautoload pathを追加しない(Zeitwerk有効時false推奨)
     config.add_autoload_paths_to_load_path = false
 
+    # Cookieを処理するmeddlewareを追加
+    config.middleware.use ActionDispatch::Cookies
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -30,7 +30,32 @@ class ActiveSupport::TestCase
 
   # アクティブなユーザを返す
   def active_user
-    # User.find_by(activated: true)
-    User.find_by(id: 1)
+    # User.find_by(id: 1)
+    User.find_by(activated: true)
+  end
+
+  # api path
+  def api(path = "/")
+    "/api/v1#{path}"
+  end
+
+  # 認可ヘッダ
+  def auth(token)
+    { Authorization: "Bearer #{token}" }
+  end
+
+  # 引数のparamsでログインを行う
+  def login(params)
+    post api("/auth_token"), xhr: true, params: params
+  end
+
+  # ログアウトapi
+  def logout
+    delete api("/auth_token"), xhr: true
+  end
+
+  # レスポンスJSONをハッシュで返す
+  def res_body
+    JSON.parse(@response.body)
   end
 end
