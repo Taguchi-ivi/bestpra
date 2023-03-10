9655af20c466e93b412785c49f6f517d1b15fe48
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-03-10 11:36:08 +0900

    notification middleware fix #46

--- frontend/middleware/get-notification.js
+++ frontend/middleware/get-notification.js
@@ -1,19 +1,13 @@
-export default async ({ $auth, store, $axios }) => {
+export default async ({ store, $axios }) => {
 
     // ログイン済みの場合のみ実行
     if ($auth.loggedIn()) {
-        // ユーザーのいいね一覧が存在しない場合取得
-        const currentLiked = store.getters['modules/like/getCurrentLike'].concat()
-        const allLiked = store.getters['modules/like/getAllLike'].concat()
-        if (currentLiked.includes('nothing') || allLiked.includes('nothing')) {
-            await $axios.$get('/api/v1/articles/current_liked')
-                .then((res => {
-                    // console.log('返却データhello',res)
-                    store.commit('modules/like/setCurrentLike', res.currentLiked)
-                    const result = res.likes.map(article => ({id: article.id, likes: article.likes}))
-                    store.commit('modules/like/setAllLike', result)
-                }))
-                .catch(err => console.log(err))
-        }
+        // ページ遷移の度に通知を受け取る
+        await $axios.$get('/api/v1/notifications/read')
+            .then((res => {
+                // console.log('返却データhello',res)
+                store.commit('modules/notification/setNotificationHeader', res)
+            }))
+            .catch(err => console.log(err))
     }
 }
