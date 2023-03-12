4159373c0a9c06257158cea5cfa73b6f03b11d30
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-03-12 11:22:23 +0900

    ckeditor fix && file folder fix

--- frontend/plugins/ckeditor.js
+++ frontend/plugins/ckeditor.js
@@ -1,12 +1,3 @@
 import Vue from 'vue';
-// import CKEditor from '@ckeditor/ckeditor5-vue2';
-// import Ckeditor from "@/components/Ckeditor";
-
-// --
-import Ckeditor from '../components/Ckeditor.vue';
-// --
-// import '@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
-// import '@ckeditor/ckeditor5-vue2/dist/ckeditor.js'
-
-// Vue.use(CKEditor);
+import Ckeditor from '../components/Atom/Ckeditor.vue';
 Vue.component('Ckeditor', Ckeditor);
