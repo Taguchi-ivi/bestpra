9cefa20b732f30906ef90ad358e83bd3ea0b1ba3
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-03-16 19:48:21 +0900

    rspec test relationship model #56

--- backend/spec/models/relationship_spec.rb
+++ backend/spec/models/relationship_spec.rb
@@ -18,15 +18,76 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
 
-  # test 'should follow and unfollow a user' do
-  #   assert_not @user_first.following?(@user_second)
-  #   @user_first.follow(@user_second)
-  #   assert @user_first.following?(@user_second)
-  #   assert @user_second.followers.include?(@user_first)
-  #   @user_first.unfollow(@user_second)
-  #   assert_not @user_first.following?(@user_second)
-  #   # ユーザーは自分自身をフォローできない
-  #   @user_first.follow(@user_first)
-  #   assert_not @user_first.following?(@user_first)
-  # end
+  describe 'associations' do
+    context 'テーブルと正しく紐づいているか' do
+      it 'followersテーブル' do
+        rel = described_class.reflect_on_association(:follower)
+        expect(rel.macro).to eq :belongs_to
+      end
+
+      it 'followingテーブル' do
+        rel = described_class.reflect_on_association(:followed)
+        expect(rel.macro).to eq :belongs_to
+      end
+    end
+  end
+
+  describe 'correct_relationships' do
+    context '正しい値の場合' do
+      it 'relationshipを作成できる' do
+        following = create(:user)
+        follower = create(:user)
+        relationship = build(:relationship, follower: follower, followed: following)
+        relationship.save
+        expect(relationship).to be_valid
+      end
+    end
+  end
+
+  describe 'validate_presence' do
+    context 'followed_idがnilの場合' do
+      let!(:relationship) { build(:relationship, follower_id: nil) }
+      it 'relationshipが作成できない' do
+        following = create(:user)
+        relationship = build(:relationship, follower: nil, followed: following)
+        relationship.save
+        expect(relationship).to be_invalid
+      end
+    end
+
+    context 'follower_idがnilの場合' do
+      let!(:relationship) { build(:relationship, followed_id: nil) }
+      it 'relationshipが作成できない' do
+        follower = create(:user)
+        relationship = build(:relationship, follower: follower, followed: nil)
+        relationship.save
+        expect(relationship).to be_invalid
+      end
+    end
+  end
+
+  describe "follow and unfollow a user" do
+    before do
+      @user_first = create(:user)
+      @user_second = create(:user)
+    end
+
+    it "should follow and unfollow a user" do
+      # フォローしていないことを確認
+      expect(@user_first.following?(@user_second)).to be_falsey
+
+      # フォローしてフォローできていることを確認
+      @user_first.follow(@user_second)
+      expect(@user_first.following?(@user_second)).to be_truthy
+      expect(@user_second.followers).to include(@user_first)
+
+      # フォローを解除して解除できていることを確認
+      @user_first.unfollow(@user_second)
+      expect(@user_first.following?(@user_second)).to be_falsey
+
+      # 自分自身をフォローできないことを確認
+      @user_first.follow(@user_first)
+      expect(@user_first.following?(@user_first)).to be_falsey
+    end
+  end
 end
