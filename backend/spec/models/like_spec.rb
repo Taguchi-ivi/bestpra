d2ec95b18e6288fdee668cf1175fa087973e0263
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-03-16 18:58:46 +0900

    rspec test like model #56

--- backend/spec/models/like_spec.rb
+++ backend/spec/models/like_spec.rb
@@ -21,5 +21,50 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    context 'テーブルと正しく紐づいているか' do
+      it 'Userテーブル' do
+        rel = described_class.reflect_on_association(:user)
+        expect(rel.macro).to eq :belongs_to
+      end
+
+      it 'Articleテーブル' do
+        rel = described_class.reflect_on_association(:article)
+        expect(rel.macro).to eq :belongs_to
+      end
+    end
+  end
+
+  describe 'correct_like' do
+    context '正しい値の場合' do
+      it 'likeが作成されること' do
+        user = create(:user)
+        article = create(:article)
+        like = create(:like, user: user, article: article)
+        expect(like).to be_valid
+      end
+    end
+  end
+
+  describe 'validate_presence' do
+    context 'userが存在しない場合' do
+      let!(:article) { create(:article) }
+      let!(:like) { build(:like, user: nil, article: article) }
+      it 'likeが作成されないこと' do
+        like.save
+        expect(like).to be_invalid
+      end
+    end
+
+    context 'articleが存在しない場合' do
+      let!(:user) { create(:user) }
+      let!(:like) { build(:like, user: user, article: nil) }
+      it 'likeが作成されないこと' do
+        like.save
+        expect(like).to be_invalid
+      end
+    end
+  end
+
 end
