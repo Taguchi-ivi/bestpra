429e5a307e3aa4ed27155ef730b0311dea50c4dc
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-03-16 18:49:17 +0900

    rspec test level_list model #56

--- backend/spec/models/level_list_spec.rb
+++ backend/spec/models/level_list_spec.rb
@@ -25,4 +25,32 @@ RSpec.describe LevelList, type: :model do
       end
     end
   end
+
+  describe 'correct_level_list' do
+    context '正しく指定されている場合' do
+      let!(:level_list) { create(:level_list) }
+      it 'level_listが登録される' do
+        expect(level_list).to be_valid
+      end
+    end
+  end
+
+  describe 'validate_presence' do
+    context 'nameの値が空の場合' do
+      it 'level_listが作成されない' do
+        level_list = build(:level_list, name: '')
+        expect(level_list).to be_invalid
+      end
+    end
+  end
+
+  describe 'validate_uniqueness' do
+    context 'nameの値が重複している場合' do
+      it 'level_listが作成されない' do
+        create(:level_list, name: '小学生')
+        level_list = build(:level_list, name: '小学生')
+        expect(level_list).to be_invalid
+      end
+    end
+  end
 end
