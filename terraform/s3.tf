97085fda9f86ab35e3852d97b1d8515f689feac1
Taguchi-ivi <k.taguchi@ivisystem.com>
2023-03-22 15:14:55 +0900

    terraform s3 cors rule fix #61

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,7 +10,15 @@
 resource "aws_s3_bucket" "public_bucket" {
     bucket        = "${local.app_name}-public-s3-access-bucket"
     force_destroy = true
+}
 
+resource "aws_s3_bucket_acl" "public_bucket_acl" {
+    bucket = aws_s3_bucket.public_bucket.id
+    acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "public_bucket_cors" {
+    bucket = aws_s3_bucket.public_bucket.id
     cors_rule {
         allowed_origins = ["https://${local.api_domain_name}"]
         # allowed_methods = ["GET","PUT", "POST"]
@@ -18,9 +26,4 @@ resource "aws_s3_bucket" "public_bucket" {
         allowed_headers = ["*"]
         max_age_seconds = 3600
     }
-}
-
-resource "aws_s3_bucket_acl" "public_bucket_acl" {
-    bucket = aws_s3_bucket.public_bucket.id
-    acl    = "public-read"
 }
